1a68627967fc1c91775930a872b27a0eaa58dae4
Fabien Thouny <fthouny@excilys.com>
2015-03-24 01:10:01 +0100

    Reviews Stock interface + Adds test harness

--- src/main/java/counter/Stock.java
+++ src/main/java/counter/Stock.java
@@ -1,11 +1,64 @@
 package counter;
 
+import java.util.List;
+
 public interface Stock {
 
-    void book(long amount) throws Exception;
+    long book() throws StoreEmptyException;
 
-    void provision(long amount);
+    Provision provision(long amount);
+
+    void refund(long amountNotConsumed);
 
     long remainingAmount();
 
+    Bill bill(String consumer, long amount);
+
+    List<Bill> bills();
+
+    List<Provision> provisions();
+
+    public static class StoreEmptyException extends Exception {
+
+    }
+
+    static class Bill {
+        private String consumer;
+        private long amount;
+
+        public Bill(String consumer, long amount) {
+            this.amount = amount;
+            this.consumer = consumer;
+        }
+
+        public String getConsumer() {
+            return consumer;
+        }
+
+        public long getAmount() {
+            return amount;
+        }
+
+        @Override
+        public String toString() {
+            return "Bill [consumer=" + consumer + ", amount=" + amount + "]";
+        }
+    }
+
+    static class Provision {
+        private long amount;
+
+        public Provision(long amount) {
+            this.amount = amount;
+        }
+
+        public long getAmount() {
+            return amount;
+        }
+
+        @Override
+        public String toString() {
+            return "Provision [amount=" + amount + "]";
+        }
+    }
 }
--- src/test/java/counter/StockTest.java
+++ src/test/java/counter/StockTest.java
@@ -1,21 +1,19 @@
 package counter;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.rangeClosed;
 import static org.assertj.core.api.Assertions.assertThat;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
-import org.junit.Before;
 import org.junit.Test;
+import counter.Stock.Bill;
+import counter.Stock.Provision;
+import counter.Stock.StoreEmptyException;
 
 public class StockTest {
 
-    private Stock stock = null;
-
-    @Before
-    public void before() {
-        stock = new ThreadSafeStock();
-    }
+    private Stock stock = new ThreadSafeStock();
 
     @Test
     public void should_initial_stock_amount_be_zero() {
@@ -31,34 +29,119 @@ public class StockTest {
     @Test
     public void should_book() throws Exception {
         stock.provision(10);
-        stock.book(5);
-        assertThat(stock.remainingAmount()).isEqualTo(5);
+        long booked = stock.book();
+        assertThat(booked).isGreaterThan(0);
+        assertThat(stock.remainingAmount() + booked).isEqualTo(10);
     }
 
-
-    @Test(expected = Exception.class)
+    @Test(expected = StoreEmptyException.class)
     public void should_not_book() throws Exception {
-        stock.book(1);
+        stock.book();
     }
 
     @Test
-    public void should_not_book_more_than_provisioned_amout() throws InterruptedException {
-        ExecutorService threadPool = Executors.newFixedThreadPool(4);
-        stock.provision(10000);
-        AtomicLong booked = new AtomicLong(0);
+    public void should_not_consume_more_than_provisioned_amount() throws Exception {
 
-        for (int i = 0; i < 100000; i++) {
-            threadPool.submit(() -> {
-                try {
-                    stock.book(1);
-                    booked.incrementAndGet();
-                } catch (Exception ignored) {
-                }
-            });
+        Provisioner provisioner = new Provisioner(stock);
+        List<Consumer> consumers = rangeClosed(1, 4).boxed().map(i -> new Consumer("consumer" + i, stock)).collect(toList());
+        provisioner.start();
+        consumers.stream().forEach(Consumer::start);
+
+        TimeUnit.MILLISECONDS.sleep(10000);
+
+        provisioner.interrupt();
+        consumers.stream().forEach(Consumer::interrupt);
+
+        List<Provision> provisions = stock.provisions();
+        System.out.println("provisions : " + provisions);
+
+        List<Bill> bills = stock.bills();
+        System.out.println("bills : " + bills);
+
+        long totalProvisioned = provisions.stream().mapToLong(provision -> provision.getAmount()).sum();
+        System.out.println("totalProvisioned : " + totalProvisioned);
+
+        long totalBilled = bills.stream().mapToLong(bill -> bill.getAmount()).sum();
+        System.out.println("totalBilled : " + totalBilled);
+
+        long stock = this.stock.remainingAmount();
+        System.out.println("stock : " + stock);
+
+        assertThat(stock + totalBilled).isEqualTo(totalProvisioned);
+    }
+
+    private static class Consumer extends Thread {
+
+        private final String name;
+        private final Stock stock;
+
+        public Consumer(String name, Stock stock) {
+            this.name = name;
+            this.stock = stock;
         }
 
-        threadPool.shutdown();
-        threadPool.awaitTermination(10, TimeUnit.SECONDS);
-        assertThat(booked.get()).isEqualTo(10000);
+        @Override
+        public void run() {
+            while (true) {
+                final long toConsume = ThreadLocalRandom.current().nextLong(100);
+                long consumed = 0;
+                try {
+                    do {
+                        long booked = stock.book();
+                        long remainingToConsume = toConsume - consumed;
+                        if (remainingToConsume >= booked) {
+                            consumed += booked;
+                        } else {
+                            consumed += remainingToConsume;
+                            stock.refund(booked - remainingToConsume);
+                        }
+                    } while (consumed != toConsume);
+                } catch (StoreEmptyException e) {
+                    if (consumed > 0) {
+                        System.err.println("Comsumption ended because the stock is empty");
+                    } else {
+                        System.err.println("Comsumption not started because the stock is empty");
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(2000);
+                        } catch (InterruptedException ignored) {
+                            Thread.currentThread().interrupt();
+                            break;
+                        }
+                    }
+                }
+                if (consumed > 0) {
+                    stock.bill(name, consumed);
+                }
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException ignored) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
     }
+
+    private static class Provisioner extends Thread {
+
+        private final Stock stock;
+
+        public Provisioner(Stock stock) {
+            this.stock = stock;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                stock.provision(1000);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (InterruptedException ignored) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
+    }
+
 }
